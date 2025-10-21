c107c8285fca0133fae3117cea700d8ee10c29ed
SmollShroom <jonasito@gmx.at>
2025-10-21 19:10:18 +0200

    Update CollisionDetection.cs

--- Neonfight2/Assets/Scripts/CollisionDetection.cs
+++ Neonfight2/Assets/Scripts/CollisionDetection.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-    public WeaponController wp;
+    public weaponController wp;
     public GameObject HitParticle; 
 
         void ontriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Enemy"))
             {
-               other.GetComponent<Animator>().SetTrigger("hit");
+               other.GetComponent<Animator>().SetTrigger("hit");        //important that every 
         }
     }
 }
