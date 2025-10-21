addaad86da8eb0457c6fcc954b3897c2e00d163d
SmollShroom <jonasito@gmx.at>
2025-10-21 20:26:19 +0200

    plöbcfhijkmz79 ,.vkfz

--- Neonfight2/Assets/Scripts/CollisionDetection.cs
+++ Neonfight2/Assets/Scripts/CollisionDetection.cs
@@ -7,9 +7,13 @@ public class CollisionDetection : MonoBehaviour
 
     private void OntriggerEnter(Collider other)
     {
+<<<<<<< Updated upstream
         Debug.Log(other.name);
 
         if (other.tag=="enemy") //&& wc.isAttacking
+=======
+        if (other.tag == "enemy")
+>>>>>>> Stashed changes
         {
             Debug.Log(other.name);
             other.GetComponent<Animator>().SetTrigger("hit");        //important that every enemy has an animator with a "hit" trigger
--- Neonfight2/Assets/Scripts/weaponController.cs
+++ Neonfight2/Assets/Scripts/weaponController.cs
@@ -71,6 +71,7 @@ public class weaponController : MonoBehaviour
         isAttacking = false;
     }
 
+
 }
 
 
