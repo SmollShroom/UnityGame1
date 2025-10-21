9b4caa5a6d6f5b0153498917e80121dc0492fed5
SmollShroom <jonasito@gmx.at>
2025-10-21 20:30:39 +0200

    Update CollisionDetection.cs

--- Neonfight2/Assets/Scripts/CollisionDetection.cs
+++ Neonfight2/Assets/Scripts/CollisionDetection.cs
@@ -7,13 +7,11 @@ public class CollisionDetection : MonoBehaviour
 
     private void OntriggerEnter(Collider other)
     {
-<<<<<<< Updated upstream
         Debug.Log(other.name);
 
         if (other.tag=="enemy") //&& wc.isAttacking
-=======
+
         if (other.tag == "enemy")
->>>>>>> Stashed changes
         {
             Debug.Log(other.name);
             other.GetComponent<Animator>().SetTrigger("hit");        //important that every enemy has an animator with a "hit" trigger
